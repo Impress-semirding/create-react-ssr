84dba8b3a127d2be0cb09d1ac2b9cc17fd847aa7
senir <304875853@qq.com>
2022-01-17 19:31:08 +0800

    :bug:fix spiliting coding

--- template/index.js
+++ template/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { hydrate } from 'react-dom'
 import { BrowserRouter } from "react-router-dom";
+import { loadableReady } from '@loadable/component'
 
 import App from "/Users/dingxue/Downloads/github/my-cli/my-react-template/router.config.js";
 
@@ -8,11 +10,12 @@ import App from "/Users/dingxue/Downloads/github/my-cli/my-react-template/router
   module.hot.accept();
   }
 
-  ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-  );
+  loadableReady(() => {
+    hydrate(
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>,
+      document.getElementById('root'))
+  })
