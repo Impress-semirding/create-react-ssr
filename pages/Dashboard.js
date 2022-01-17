2aa2ec564dc1918041f04070266cf46448269070
senir <304875853@qq.com>
2022-01-17 19:38:06 +0800

    :bug:

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -5,41 +5,7 @@ export default function Dashboard() {
   // These routes are defined when this component is loaded on demand via
   // dynamic import() on the home page!
   return (
-    <Switch>
-      <Route path="/" component={<DashboardLayout />}>
-        <Route index component={<DashboardIndex />} />
-        <Route path="messages" component={<Messages />} />
-      </Route>
-    </Switch>
-  );
-}
-
-function DashboardLayout() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/dashboard">Dashboard Home</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/messages">Messages</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      <Outlet />
-    </div>
-  );
-}
-
-function DashboardIndex() {
-  return (
-    <div>
-      <h2>Dashboard Index</h2>
-    </div>
+     <Messages />
   );
 }
 
